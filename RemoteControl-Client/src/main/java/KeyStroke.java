ac506df29ed455146586a22693dbefdc09e95001
ngantruc2003 <ngantruc2003@gmail.com>
2022-12-06 23:02:19 +0700

    add UI for key stroke

--- RemoteControl-Client/src/main/java/KeyStroke.java
+++ RemoteControl-Client/src/main/java/KeyStroke.java
@@ -30,6 +30,17 @@ class KeyStroke extends JPanel implements Runnable {
     private Socket socket;
     private JFrame frame;
     private DataInputStream dataInputStream;
+    KeyStroke ppp = this;
+    User32 lib = User32.INSTANCE;
+	User32jna u32 = User32jna.INSTANCE;
+    HHOOK hhk = null;
+	HMODULE hMod = Kernel32.INSTANCE.GetModuleHandle(null);
+	Thread thread;
+	Object lock = new Object();
+    public interface User32jna extends Library {
+        KeyStroke.User32jna INSTANCE = null;
+        User32jna INSTACE = (User32jna) Native.loadLibrary("user32.dll", User32jna.class);
+    }
     public KeyStroke(JFrame frame, Socket socket) {
         setLayout(null);
         this.socket = socket;
@@ -38,11 +49,23 @@ class KeyStroke extends JPanel implements Runnable {
         hookBtn.setBounds(50,20,150,50);
         hookBtn.setFont(new Font(myFont, Font.PLAIN, 20));
         add(hookBtn);
-        JButton unHookBtn = new JButton("Hook");
+        JButton unHookBtn = new JButton("Unhook");
         unHookBtn.setBounds(220,20,150,50);
         unHookBtn.setFont(new Font(myFont, Font.PLAIN, 20));
-        add(hookBtn);
-
+        add(unHookBtn);
+        JTextArea display = new JTextArea ( 16, 58 );
+        display.setEditable ( false ); // set textArea non-editable
+        String str = "";
+        for (int i = 0; i < 50; ++i)
+            str += "Some text\n";
+        display.setText(str);
+        JScrollPane scroll = new JScrollPane ( display );
+        scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+        scroll.setBounds(20, 90, 550, 250);  
+        add(scroll);
+        //setLocation(null);
+        thread = new Thread(this);
+        thread.start();
     }
     public void run() {
 
